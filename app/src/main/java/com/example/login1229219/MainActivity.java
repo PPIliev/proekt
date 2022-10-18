a04f9ef65179f788d5d0e6555d26bde1e264335a
IlievPPavel <1229219@nvna.eu>
2022-10-18 19:48:52 +0300

    1.5/2

--- app/src/main/java/com/example/login1229219/MainActivity.java
+++ app/src/main/java/com/example/login1229219/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.login1229219;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,7 +22,20 @@ public class MainActivity extends AppCompatActivity {
         b_login = findViewById(R.id.b_login);
         b_register = findViewById(R.id.b_register);
 
+        b_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goRegister();
+            }
+        });
 
 
+
+    }
+
+    public void goRegister() {
+        Intent i = new Intent(getApplicationContext(), Register.class);
+//        i.putExtra("info",)
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/login1229219/Register.java
+++ app/src/main/java/com/example/login1229219/Register.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     Button b_register;
     EditText et_password2, et_password, et_username, et_email;
+    TextView tv_passError;
+
+    Dbhelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,28 @@ public class Register extends AppCompatActivity {
         et_password2 = findViewById(R.id.et_password2);
         et_username = findViewById(R.id.et_username);
         et_email = findViewById(R.id.et_email);
+        tv_passError = findViewById(R.id.tv_passsError);
+
+        b_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email =et_email.getText().toString();
+                if (et_password.getText().toString().equals(et_password2.getText().toString())) {
+                    if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    UsersModel usersModel = new UsersModel(-1, et_username.getText().toString() ,et_password.getText().toString(),et_email.getText().toString());
+                    Dbhelper dbHelper = new Dbhelper(getApplicationContext());
+                    dbHelper.insertUser(usersModel);
+                } else {
+                        //email
+                        tv_passError.setText("You must enter a valid email!");
+                        tv_passError.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    //password
+                    tv_passError.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 
     }
